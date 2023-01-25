a5d9a660d604d1b2d2b1c5902a43419b3ef1b05d
LeonardRingot <leonardringot6@gmail.com>
2023-01-25 17:02:21 +0100

    no idea

--- mapper/plage_horaire.mapper.ts
+++ mapper/plage_horaire.mapper.ts
@@ -5,20 +5,15 @@ export class PlageHoraireMapper {
     static mapToDto(plage_horaire: Plage_Horaire | null): plageHoraireDTO  {
         if (plage_horaire === null) return null as any;
         return {
-<<<<<<< HEAD
+
             jour:plage_horaire.jour,
             StartHour:plage_horaire.StartHour,
             EndHour:plage_horaire.EndHour,
             pauseStart:plage_horaire.pauseStart,
             pauseEnd:plage_horaire.pauseEnd,
             duree_horaire:plage_horaire.duree_horaire,
-=======
-            date:plage_horaire.date,
-            startHour:plage_horaire.startHour,
-            endHour:plage_horaire.endHour,
-            pauseStartHour:plage_horaire.pauseStartHour,
-            pauseEndHour:plage_horaire.pauseEndHour,
->>>>>>> bb5ac0482451c8167aa80e629380b973acfd336e
+
+        
         }
     }
 
